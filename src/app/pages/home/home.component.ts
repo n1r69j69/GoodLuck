e78ac41e6b5029635a3f670b5bda3d8cb7934a92
n1r69j69 <niranjankumar91r69j68@gmail.com>
2023-06-02 17:44:21 +0530

    about

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,4 +9,30 @@ export class HomeComponent {
   public data= "Hello World";
 public color="text-primary";
 public data1 =true;
+public data3= [
+  {
+    name:'tum',
+    id: 1,
+  },
+  {
+    name:'ha',
+    id: 2,
+  },
+  {
+    name:'yes',
+    id: 3,
+  },
+  {
+    name:'tum',
+    id: 4,
+  },
+  {
+    name:'me',
+    id: 5,
+  },
+  {
+    name:'ho',
+    id: 6,
+  },
+];
 }
