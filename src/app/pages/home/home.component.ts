3d1bebf387f3c3e54048a8ea0acea2a006eae36d
n1r69j69 <niranjankumar91r69j68@gmail.com>
2023-06-03 18:30:24 +0530

    card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
- 
+  public productDetails='';
+  public getProductDetail(product:any){
+    console.log(product);
+    this.productDetails=product;
+  }
+ public products= [
+{
+  id:1,
+  name: 'product 1',
+  price: '1000',
+  description:
+  'This is a wider card with supporting text bellow as a naturalled-in to additional content.',
+  imgUrl: 'assets/images/home/img.jpg',
+},
+{
+  id:2,
+  name: 'product 2',
+  price: '100',
+  description:
+  'This is a wider card with supporting text bellow as a naturalled-in to additional content.',
+  imgUrl: 'assets/images/home/img.jpg',
+},
+{
+  id:3,
+  name: 'product 3',
+  price: '10',
+  description:
+  'This is a wider card with supporting text bellow as a naturalled-in to additional content.',
+  imgUrl: 'assets/images/home/img.jpg',
+},
+{
+  id:4,
+  name: 'product 4',
+  price: '1',
+  description:
+  'This is a wider card with supporting text bellow as a naturalled-in to additional content.',
+  imgUrl: 'assets/images/home/img.jpg',
+},
+{
+  id:5,
+  name: 'product 5',
+  price: '10000',
+  description:
+  'This is a wider card with supporting text bellow as a naturalled-in to additional content.',
+  imgUrl: 'assets/images/home/img.jpg',
+},
+
+ ];
 }
